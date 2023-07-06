15d8bf547a53911ba54f32b863942d52ad492cd2
VAIBHAV <68508494+vaibhavd2103@users.noreply.github.com>
2023-07-06 12:05:45 +0530

    config for mongoDB

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,6 @@
 const config = {
 	port: 5000,
-	dbUrl:
-		"mongodb+srv://admin:vaibhavd2103@cluster0.darmpbn.mongodb.net/?retryWrites=true&w=majority",
+	dbUrl: "mongodb+srv://admin:dange2103@cluster0.3olsdnt.mongodb.net/",
 };
 
 module.exports = config;
